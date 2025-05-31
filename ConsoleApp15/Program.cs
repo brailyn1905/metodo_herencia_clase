c423a1bb1da967efee0fd86bc14900a33f17e3df
Brailyn Jaquez <ramirezbrailyn55@hotmail.com>
2025-05-31 14:56:21 -0800

    Add project files.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace practica
+{
+    public class rentcard
+    {
+        public string marca;
+        public string tipo;
+        public string ano;
+
+
+    }
+
+    public class Alquiler : rentcard
+
+    {
+        public string nombre;
+        public string tiempoAlquiler;
+
+           public void pedirdatos()
+        {
+            Console.WriteLine("escriba su nombre");
+            string nombre=Console.ReadLine();
+            Console.WriteLine("tiempo de renta ");
+            int dias  = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("tipo de vehiculo ");
+            string tipo = Console.ReadLine();
+
+            Console.WriteLine("los datos obtenidos son:"+"lo rento " + nombre + "durante  " + dias + " dias "+","+" del tipo  "+tipo );
+
+        }
+        
+
+
+    }
+    
+   
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Alquiler miAlquiler = new Alquiler();
+            miAlquiler.pedirdatos();
+
+
+
+
+        }
+
+
+
+
+    }
+
+}
